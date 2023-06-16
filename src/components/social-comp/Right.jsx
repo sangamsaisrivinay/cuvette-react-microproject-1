287a41e16cf0b3e784f48b28c68909ff6aeba1ed
winay729 <sssvinayr@gmail.com>
2023-06-16 21:05:00 +0530

    completed bottom section

--- src/components/social-comp/Right.jsx
+++ src/components/social-comp/Right.jsx
@@ -1,8 +1,25 @@
+
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+
 
 const Right = () => {
   return (
-    <div>Right</div>
+    <div className="container small column-flex" style={{"justifyContent":"space-between"}}>
+      <div className='captz small-text'>
+        <div>dweep.io</div>
+        <div>made with love in india</div>
+        <div className='container row-flex' style={{"justifyContent":"flex-end"}}>
+          <FontAwesomeIcon icon={faLinkedin} className='social-icons' />
+          <FontAwesomeIcon icon={faInstagram} className='social-icons'/>
+        </div>
+      </div>
+      <div className='small-text'>
+          hello@dweep.io
+      </div>
+    </div>
   )
 }
 
